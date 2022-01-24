31724b91403f2051a70612b71fec3180e6a2f0f5
usmanmahmood940 <usman.hussain940@gmail.com>
2022-01-24 06:45:52 +0500

    Usman change 2.0 ( Feature added : SlideAble and ClickAble Fragments + Theme changed)

---
+++ app/src/main/java/com/example/food/FragmentAdapter.java
@@ -0,0 +1,41 @@
+package com.example.food;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentAdapter extends FragmentStatePagerAdapter {
+
+    int noOfFragments;
+    public FragmentAdapter(@NonNull FragmentManager fm, int numberOfFragments) {
+
+        super(fm);
+        this.noOfFragments = numberOfFragments;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) { //position default value is zero
+        if(position == 0){
+            PopularFragment fragmentOne = new PopularFragment();
+            return fragmentOne;
+        }
+//        else if(position == 1){
+//            Fragment fragmentTwo = new BestFragment();
+//            return fragmentTwo;
+//        }
+        else{
+            Fragment fragmentTwo = new BestFragment();
+            return fragmentTwo;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }   // slide count
+}
--- app/src/main/java/com/example/food/MainActivity.java
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.food;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -14,6 +16,7 @@ import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,14 +27,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager pager;
+    FragmentAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        frag(1);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(getResources().getString(R.string.app_name));
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        pager = (ViewPager)findViewById(R.id.pager);
+        tabLayout.addTab(tabLayout.newTab().setText("Popular"));
+        tabLayout.addTab(tabLayout.newTab().setText("Burgers"));
+        tabLayout.addTab(tabLayout.newTab().setText("Pizzas"));
+
+        FragmentManager fm = getSupportFragmentManager();
+        adapter = new FragmentAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        pager.setAdapter(adapter);
+        pager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
-    public void popular(View view) {
+    /*public void popular(View view) {
         frag(1);
     }
 
@@ -53,5 +93,5 @@ public class MainActivity extends AppCompatActivity{
         fragmentTransaction.replace(R.id.frame,pop);
         fragmentTransaction.commit(); // Save changes
     }
-
+*/
 }
